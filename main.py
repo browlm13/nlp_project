2201d51d40fe0718bf3e81518b7ee2665ba88715
browlm13 <browlm13@gmail.com>
2017-12-02 02:20:18 -0600

    scrapping

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from src.text_editing.co_occurrence_matrix_v2 import Coo_Matrix
 import os
 
 # settings
-subreddit = 'worldnews'
+subreddit = 'news'
 subreddit_data_path = '/data/data_sets/scraped_subreddits'
 
 def scrape_reddit(subreddit, subreddit_data_path):
@@ -23,7 +23,7 @@ def load_dataset(subreddit, subreddit_data_path):
 #
 #	test scrape reddit
 #
-#scrape_reddit(subreddit, subreddit_data_path)
+scrape_reddit(subreddit, subreddit_data_path)
 
 
 #
@@ -45,17 +45,15 @@ def load_dataset(subreddit, subreddit_data_path):
 #
 
 #directory_path = '/data/models'
-#model_name = 'model1'
+#model_name = 'model2'
 #CM.save(directory_path,model_name)
 
 # 
 # test load coocurence matrix
 #
 
-directory_path = '/data/models'
-model_name = 'model1'
-CM = Coo_Matrix()
-CM.load(directory_path + '/' + model_name)
+#CM = Coo_Matrix()
+#CM.load(directory_path + '/' + model_name)
 
 
 
@@ -76,9 +74,6 @@ print(CM.rank_most_related_words('donald')[:20])
 trump_list = CM.related_words_list_filtered_decending('trump')
 print(trump_list[:20])
 
-"""
-
-
 #
 #	Save results
 #
@@ -92,8 +87,8 @@ m = 10		# top m most related words
 random_indexs = random.sample(range(0, len(id2w)-1), n)
 corpus_terms = [id2w[i] for i in random_indexs]
 
-corpus_terms = ['republican','flynn','trump','2016','lied','biblical','drug','communist','criminal','russia','chinese','inauguration','obama','nuclear','spine','twitter','students','unanimous','hillary','peace','deal', 'clearances','financial','failed','prescription','unprecedented','election','ship','taliban','military']
-corpus_terms = ['republican','ship','drug','communist','taliban','flynn','russia','inauguration','trump','obama','spine','2016','twitter','unprecedented','election','unanimous','hillary','prescription','peace','financial']
+#corpus_terms = ['republican','flynn','trump','2016','lied','biblical','drug','communist','criminal','russia','chinese','inauguration','obama','nuclear','spine','twitter','students','unanimous','hillary','peace','deal', 'clearances','financial','failed','prescription','unprecedented','election','ship','taliban','military']
+#corpus_terms = ['republican','ship','drug','communist','taliban','flynn','russia','inauguration','trump','obama','spine','2016','twitter','unprecedented','election','unanimous','hillary','prescription','peace','financial']
 
 # list of lists where the first index is a corpus term and the trailing indexs are its ranked related words
 list_of_related_word_lists = []
@@ -110,7 +105,7 @@ np_related_words = np.array(list_of_related_word_lists)
 directory_path = 'results/cooccurence_matrix_related_words'
 np_related_words_file_path = directory_path + '/np_related_words_'+ model_name	#np.save appends extension .npy
 np.save(np_related_words_file_path, np_related_words)
-
+"""
 
 #corpus_terms = ['donald', 'trump', 'america', 'russia', 'space','cannabis','prescription','court']
 #llist = CM.related_words_list_filtered_decending('space')
